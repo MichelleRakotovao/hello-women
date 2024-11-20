8492baa88134c74c09475530cf4309ea2985a48d
RAKOTOVAO Michelle <rakotovaomichelle7@gmail.com>
2024-11-20 21:54:42 +0300

    configuration web

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule { }
